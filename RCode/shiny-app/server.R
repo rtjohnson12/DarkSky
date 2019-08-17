11474fcac3566ddf53ff2c9ff96bc715b07d65c7
Ryan Johnson <rjohnjam@gmail.com>
2019-08-17 11:11:46 -0700

    added city data as global variable

--- RCode/shiny-app/server.R
+++ RCode/shiny-app/server.R
@@ -57,26 +57,83 @@ shinyServer(function(input, output) {
     
     output$WorldMap <- renderLeaflet({
         
-        # dataframe for markers
-        visit.data <- data.frame(
-            City = c("Seattle", "Santa Clara", "San Francisco"),
-            State = c("Washington", "California", "California"),
-            Country = c("US", "US", "US"),
-            Long = c(-122.3320708,-121.955238,-122.4194),
-            Lat = c(47.6062095,37.354107,37.77493),
-            n = c(100, 60, 10)
-        ) %>% tibble::add_row(
-            City = "Portland", State = "OR", Country = "US",
-            Long = -122.676482, Lat = 45.523062, n = 10
+        # icon
+        greenLeafIcon <- makeIcon(
+            iconUrl = "http://leafletjs.com/examples/custom-icons/leaf-green.png",
+            iconWidth = 38, iconHeight = 95,
+            iconAnchorX = 22, iconAnchorY = 94,
+            shadowUrl = "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+            shadowWidth = 50, shadowHeight = 64,
+            shadowAnchorX = 4, shadowAnchorY = 62
         )
         
-        # owmr::search_city_list("", country_code = "US")
+        # dataframe for markers
+        city.data <- data.frame(
+            City = character(), State = character(), Country = character(),
+            Long = numeric(), Lat = numeric()
+        ) %>% tibble::add_row(
+            City = "Seattle", State = "WA", Country = "US",
+            Long = -122.3320708, Lat = 47.6062095
+        ) %>% tibble::add_row(
+            City = "Santa Clara", State = "CA", Country = "US",
+            Long = -121.955238, Lat = 37.354107
+        ) %>% tibble::add_row(
+            City = "San Francisco", State = "CA", Country = "US",
+            Long = -122.4194, Lat = 37.77493
+        ) %>% tibble::add_row(
+            City = "Portland", State = "OR", Country = "US",
+            Long = -122.676482, Lat = 45.523062
+        ) %>% tibble::add_row(
+            City = "Bellevue", State = "WA", Country = "US",
+            Long = -122.20068, Lat = 47.61038
+        ) %>% tibble::add_row(
+            City = "Federal Way", State = "WA", Country = "US",
+            Long = -122.3126, Lat = 47.32232
+        ) %>% tibble::add_row(
+            City = "Sacremento", State = "CA", Country = "US",
+            Long = -121.4944, Lat = 38.58157
+        ) %>% tibble::add_row(
+            City = "Nampa", State = "ID", Country = "US",
+            Long = -116.5635, Lat = 43.54072
+        ) %>% tibble::add_row(
+            City = "Salt Lake City", State = "UT", Country = "US",
+            Long = -111.8911, Lat = 40.76078
+        ) %>% tibble::add_row(
+            City = "Spokane", State = "WA", Country = "US",
+            Long = -117.4291, Lat = 47.65966
+        ) %>% tibble::add_row(
+            City = "Colorado Springs", State = "CO", Country = "US",
+            Long = -104.8214, Lat = 38.83388
+        ) %>% tibble::add_row(
+            City = "Marshalltown", State = "IO", Country = "US",
+            Long = -92.90798, Lat = 42.04943
+        ) %>% tibble::add_row(
+            City = "Jacksonville", State = "FL", Country = "US",
+            Long = -81.655647, Lat = 30.332184
+        ) %>% tibble::add_row(
+            City = "Freiburg", State = NA, Country = "DE",
+            Long = 7.85222, Lat = 47.9959
+        ) %>% tibble::add_row(
+            City = "Berlin", State = NA, Country = "DE",
+            Long = 13.41053 , Lat = 52.52437
+        ) %>% tibble::add_row(
+            City = "Frankfurt", State = NA, Country = "DE",
+            Long = 8.68333, Lat = 50.11667
+        ) %>% tibble::add_row(
+            City = "Strasbourg", State = NA, Country = "FR",
+            Long = 7.74296, Lat = 48.58342
+        )
+        
+        # %>% tibble::add_row(
+        #     City = "asdf", State = "asdf", Country = "asdf",
+        #     Long = asdf, Lat = asdf
+        # )
+        
+        # owmr::search_city_list("Frankfurt", country_code = "DE")
         
         # map setup
-        m <- leaflet(visit.data) %>% addTiles() %>% 
-            addCircles(lng = ~Long, lat = ~Lat, weight = 1,
-                       radius = ~n * 1000, popup = ~City
-            )
+        m <- leaflet(visit.data %>% dplyr::filter(Country == "US")) %>% addTiles() %>% 
+            addMarkers(lng = ~Long, lat = ~Lat, popup = ~City)
         
         # render map
         m
